c39f29d2af8030239e57e4979a90c0c849c9c82c
kev <kevin-jovan.japou-bopa.1@ulaval.ca>
2026-04-28 10:13:14 -0400

    style(global): Setup CSS globals et entry point React
    - Global styles avec Tailwind directives (@base, @components, @utilities)
    - Scrollbar minimaliste et focus-visible pour accessibilité
    - Root application avec BrowserRouter, LearningProvider
    - Support des vidéos et assets statiques

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+﻿import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { BackgroundOrbs } from './components/layout/background-orbs';
+import { LearningProvider } from './context/learning-context';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <LearningProvider>
+        <BackgroundOrbs />
+        <App />
+      </LearningProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
